899f65211ad72a1e3cc610fa18cc75daac781892
EC2 Default User <ec2-user@ip-172-31-21-81.ap-northeast-1.compute.internal>
2022-08-14 08:03:25 +0000

    8月14日分

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profile/index'
+  get 'web/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "web#index"
 end
